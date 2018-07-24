6cf9c2ab8cc09c4db69910981dd15f5322130f67
Preety Kumari <preetydakshana@gmail.com>
2018-07-24 16:43:08 +0530

    code updated

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,15 +6,10 @@ namespace Transform
 {
     public static class Transform 
     { 
-     public delegate int delegatesquare(int num); 
-        public static List<int> Map(this int[] a,delegatesquare square)
+     
+        public static IEnumerable<TResult>  Map<TSource , TResult>(this IEnumerable<TSource> x,Func<TSource,TResult> Transform)
         {
-           List<int> number =new List<int>();
-			foreach (int x in a)
-            {
-				number.Add(square(x));
-            }
-			return number;
+           return  x.Select(Transform);
         }
     }
 }
